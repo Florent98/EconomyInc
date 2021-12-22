1abf1a650b5a829596f56bf500612e8e65ec2aad
MarcusSlover <mariuszek016@gmail.com>
2021-12-22 13:13:07 +0100

    + API + credits

--- src/main/java/fr/fifoube/main/capabilities/EconomyAPI.java
+++ src/main/java/fr/fifoube/main/capabilities/EconomyAPI.java
@@ -1,17 +1,39 @@
 package fr.fifoube.main.capabilities;
 
+import com.google.common.util.concurrent.AtomicDouble;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
 import java.util.UUID;
 
 public class EconomyAPI {
     private EconomyAPI() {
-
     }
 
     public static double getBalance(UUID uuid) {
+        ServerPlayerEntity serverPlayerEntity = getPlayerByUUID(uuid);
+        if (serverPlayerEntity == null) return -1;
 
+        AtomicDouble atomicDouble = new AtomicDouble(0);
+        serverPlayerEntity.getCapability(CapabilityMoney.MONEY_CAPABILITY, null).ifPresent(data -> atomicDouble.set(data.getMoney()));
+        return atomicDouble.doubleValue();
     }
 
     public static void setBalance(UUID uuid, double money) {
+        ServerPlayerEntity serverPlayerEntity = getPlayerByUUID(uuid);
+        if (serverPlayerEntity == null) return;
+        serverPlayerEntity.getCapability(CapabilityMoney.MONEY_CAPABILITY, null).ifPresent(data -> {
+            data.setMoney(money);
+        });
+    }
 
+    public static ServerPlayerEntity getPlayerByUUID(UUID uuid) {
+        MinecraftServer currentServer = ServerLifecycleHooks.getCurrentServer();
+        if (currentServer == null) return null;
+
+        PlayerList playerList = currentServer.getPlayerList();
+        return playerList.getPlayerByUUID(uuid);
     }
 }
